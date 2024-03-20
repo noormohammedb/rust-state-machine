16362ea558785eb812d0a29a04182276a25ec932
Noor Mohammed <noormohammedb@protonmail.com>
2024-03-20 11:47:23 +0400

    balance transfer with its tests

--- src/balances.rs
+++ src/balances.rs
@@ -29,6 +29,26 @@ impl Pallet {
 	pub fn balance(&self, who: &String) -> u128 {
 		*self.balances.get(who).unwrap_or(&0)
 	}
+
+	pub fn transfer(
+		&mut self,
+		from: &String,
+		to: &String,
+		amount: u128,
+	) -> Result<(), &'static str> {
+		let from_balance = self.balance(from);
+
+		let to_balance = self.balance(to);
+
+		let updated_from = from_balance.checked_sub(amount).ok_or("Insufficient balance")?;
+
+		let updated_to = to_balance.checked_add(from_balance - updated_from).ok_or("Overflow")?;
+
+		self.set_balance(from, updated_from);
+		self.set_balance(to, updated_to);
+
+		Ok(())
+	}
 }
 
 #[cfg(test)]
@@ -43,5 +63,25 @@ mod tests {
 		balances.set_balance(&"alice".to_string(), 100);
 		assert_eq!(balances.balance(&"alice".to_string()), 100);
 		assert_eq!(balances.balance(&"bob".to_string()), 0);
+	}
+
+	#[test]
+	fn transfer_balance() {
+		let mut balances = Pallet::new();
+
+		let alice = &"alice".to_string();
+		let bob = &"bob".to_string();
+		let insufficient_err = Err("Insufficient balance");
+
+		assert_eq!(balances.transfer(alice, bob, 57), insufficient_err);
+
+		balances.set_balance(alice, 45);
+
+		assert_eq!(balances.balance(bob), 0);
+
+		assert_eq!(balances.transfer(alice, bob, 44), Ok(()));
+
+		assert_eq!(balances.balance(alice), 1);
+		assert_eq!(balances.balance(bob), 44);
 	}
 }
