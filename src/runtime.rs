7fa077c7fc9214aea25148f7b49d60f07010ab58
Noor Mohammed <noormohammedb@protonmail.com>
2024-03-25 21:08:50 +0400

    Customized System Pallet to Configurable
    Pallet Config trait bound over generic types for pallets
    updated tests to compatible with pallet runtime

--- src/runtime.rs
+++ src/runtime.rs
@@ -5,7 +5,7 @@ use crate::{
 
 #[derive(Debug)]
 pub struct Runtime {
-	pub system: system::Pallet<BlockNumber, AccountId, Nonce>,
+	pub system: system::Pallet<Self>,
 	pub balances: balances::Pallet<AccountId, Balance>,
 }
 
@@ -13,4 +13,10 @@ impl Runtime {
 	pub fn new() -> Self {
 		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
 	}
+}
+
+impl system::Config for Runtime {
+	type AccountId = AccountId;
+	type BlockNumber = BlockNumber;
+	type Nonce = Nonce;
 }
--- src/system.rs
+++ src/system.rs
@@ -1,23 +1,24 @@
 use num::{CheckedAdd, One, Unsigned, Zero};
 use std::{collections::BTreeMap, ops::AddAssign};
 
-#[derive(Debug)]
-pub struct Pallet<BlockNumber, AccountId, Nonce> {
-	block_number: BlockNumber,
-	nonce: BTreeMap<AccountId, Nonce>,
+pub trait Config {
+	type BlockNumber: Zero + One + CheckedAdd + Unsigned + AddAssign + Copy;
+	type AccountId: Ord + Clone;
+	type Nonce: Copy + Zero + One;
 }
 
-impl<BlockNumber, AccountId, Nonce> Pallet<BlockNumber, AccountId, Nonce>
-where
-	BlockNumber: Zero + One + CheckedAdd + Unsigned + AddAssign + Copy,
-	AccountId: Ord + Clone,
-	Nonce: Zero + One + Unsigned + Copy,
-{
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+	block_number: T::BlockNumber,
+	nonce: BTreeMap<T::AccountId, T::Nonce>,
+}
+
+impl<T: Config> Pallet<T> {
 	pub fn new() -> Self {
-		Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
+		Self { block_number: T::BlockNumber::zero(), nonce: BTreeMap::new() }
 	}
 
-	pub fn _genesis_init(b_num: BlockNumber, nonce_data: Vec<(AccountId, Nonce)>) -> Self {
+	pub fn _genesis_init(b_num: T::BlockNumber, nonce_data: Vec<(T::AccountId, T::Nonce)>) -> Self {
 		let mut nonce = BTreeMap::new();
 		nonce_data.into_iter().for_each(|nonce_i| {
 			nonce.insert(nonce_i.0, nonce_i.1);
@@ -25,34 +26,51 @@ where
 		Self { block_number: b_num, nonce }
 	}
 
-	pub fn block_number(&self) -> BlockNumber {
+	pub fn block_number(&self) -> T::BlockNumber {
 		self.block_number
 	}
 
-	pub fn inc_block_number(&mut self) -> BlockNumber {
-		let new_block_num = self.block_number + BlockNumber::one();
-		self.block_number += BlockNumber::one();
+	pub fn inc_block_number(&mut self) -> T::BlockNumber {
+		let new_block_num = self.block_number + T::BlockNumber::one();
+		self.block_number += T::BlockNumber::one();
 
 		new_block_num
 	}
 
-	pub fn nonce(&self, who: &AccountId) -> Nonce {
-		self.nonce.get(who).copied().unwrap_or(Nonce::zero())
+	pub fn nonce(&self, who: &T::AccountId) -> T::Nonce {
+		self.nonce.get(who).copied().unwrap_or(T::Nonce::zero())
 	}
 
-	pub fn inc_nonce(&mut self, who: &AccountId) {
-		let current = self.nonce.get(who).copied().unwrap_or(Nonce::zero());
-		self.nonce.insert(who.clone(), current + Nonce::one());
+	pub fn inc_nonce(&mut self, who: &T::AccountId) {
+		let current = self.nonce.get(who).copied().unwrap_or(T::Nonce::zero());
+		self.nonce.insert(who.clone(), current + T::Nonce::one());
 	}
 }
 
 #[cfg(test)]
 mod tests {
-	use super::Pallet;
+	use super::{Config, Pallet};
+	use crate::types::{AccountId, BlockNumber, Nonce};
 
 	#[test]
 	fn init_system() {
-		let mut system = Pallet::<u32, String, u32>::new();
+		struct TestConfig {
+			system: Pallet<Self>,
+		}
+
+		impl TestConfig {
+			fn new() -> Self {
+				Self { system: Pallet::new() }
+			}
+		}
+
+		impl Config for TestConfig {
+			type AccountId = AccountId;
+			type BlockNumber = BlockNumber;
+			type Nonce = Nonce;
+		}
+
+		let TestConfig { mut system } = TestConfig::new();
 		let alice = &"Alice".to_string();
 
 		assert_eq!(system.block_number(), 0);
